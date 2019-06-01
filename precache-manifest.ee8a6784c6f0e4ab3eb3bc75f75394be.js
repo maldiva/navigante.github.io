2bd2aada090ddb68a04b2fae4752c0d7c8660b5f
navigante <k.navigante@gmail.com>
2019-06-01 22:59:28 +0600

    Initial commit

---
+++ precache-manifest.ee8a6784c6f0e4ab3eb3bc75f75394be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "091ae3a35c02ce6a6739c5955e30365e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "4d80054df5cbb058978e",
+    "url": "/static/css/main.d0e2cf69.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "4d80054df5cbb058978e",
+    "url": "/static/js/main.3b1294c7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
